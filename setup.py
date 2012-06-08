f09a965e0383bd02c19958339b4d00e91ddd96be
Fredrik Thulin <fredrik@yubico.com>
2012-06-08 13:52:57 +0200

    Prepare version 1.1.0.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from setuptools import setup, find_packages
 
 setup(name		= 'python-yubico',
-      version		= '1.0.0',
+      version		= '1.1.0',
       description	= 'Python code for talking to Yubico\'s YubiKeys',
       author		= 'Fredrik Thulin',
       author_email	= 'fredrik@yubico.com',
