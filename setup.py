1e01836443ded8feb48612657a91febe020a6bc5
Dain Nilsson <dain@yubico.com>
2013-08-05 16:22:55 +0200

    Use python setup.py for testing and releasing.

--- setup.py
+++ setup.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from setuptools import setup, find_packages
+from setuptools import setup
+from release import release
+import sys
 
-setup(name		= 'python-yubico',
-      version		= '1.1.0',
-      description	= 'Python code for talking to Yubico\'s YubiKeys',
-      author		= 'Fredrik Thulin',
-      author_email	= 'fredrik@yubico.com',
-      url		= 'http://www.yubico.com/',
-      license		= 'BSD',
-      packages		= ['yubico'],
-      package_dir	= {'': 'Lib'},
-      tests_require	= "nose >=0.10.0b1",
-      test_suite	= "nose.collector",
-     )
+sys.path.append('Lib')
+
+setup(
+    name='python-yubico',
+    version='1.1.1',
+    description='Python code for talking to Yubico\'s YubiKeys',
+    author='Fredrik Thulin',
+    author_email='fredrik@yubico.com',
+    maintainer='Yubico Open Source Maintainers',
+    maintainer_email='ossmaint@yubico.com',
+    url='https://github.com/Yubico/python-yubico',
+    license='BSD 2 clause',
+    packages=['yubico'],
+    package_dir={'': 'Lib'},
+    install_requires=['pyusb'],
+    setup_requires=['nose>=1.0'],
+    test_suite="nose.collector",
+    cmdclass={'release': release},
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+    ]
+)
