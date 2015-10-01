0742aac76305111c04bf24a75b048ae6acb1f7fe
Dain Nilsson <dain@yubico.com>
2015-10-01 15:51:09 +0200

    Revert "Use python-yubicommon"
    This reverts commit 920d43d3c1ad3ee72e094c214801cbb44691e9ac.

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
-# Copyright (c) 2010, 2011, 2012 Yubico AB
-# See the file COPYING for licence statement.
+#!/usr/bin/env python
 
-from yubico.yubicommon.setup import setup, get_version
+from setuptools import setup
+from release import release
+import re
+
+VERSION_PATTERN = re.compile(r"(?m)^__version__\s*=\s*['\"](.+)['\"]$")
+
+
+def get_version():
+    """Return the current version as defined by yubico/__init__.py."""
+
+    with open('yubico/yubico_version.py', 'r') as f:
+        match = VERSION_PATTERN.search(f.read())
+        return match.group(1)
 
 
 setup(
     name='python-yubico',
+    version=get_version(),
     description='Python code for talking to Yubico\'s YubiKeys',
-    version=get_version('yubico/yubico_version.py'),
-    author='Dain Nilsson',  # Original author: Fredrik Thulin
-    author_email='dain@yubico.com',
+    author='Fredrik Thulin',
+    author_email='fredrik@yubico.com',
     maintainer='Yubico Open Source Maintainers',
     maintainer_email='ossmaint@yubico.com',
     url='https://github.com/Yubico/python-yubico',
@@ -17,6 +28,7 @@ setup(
     packages=['yubico'],
     install_requires=['pyusb'],
     test_suite='test',
+    cmdclass={'release': release},
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
